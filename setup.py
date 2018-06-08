6c7a46be59984092c5fe99cc08bfcb92f409a1f6
Ajay Patel <patel.ajay285@gmail.com>
2018-06-07 18:54:32 -0700

    Add large.txt

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         exclude=[
             'tests',
             'tests.*']),
-    version='0.1.23',
+    version='0.1.24',
     description='A fast, efficient universal vector embedding utility package.',
     long_description="""
 About
