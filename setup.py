726b857dc8badb6e995ff755ef59825456b91bd9
Ajay Patel <patel.ajay285@gmail.com>
2018-06-10 21:39:29 -0700

    Remove custom egg info

--- setup.py
+++ setup.py
@@ -271,7 +271,7 @@ if __name__ == '__main__':
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7'],
         cmdclass={
-            'egg_info': CustomEggInfoCommand,
+            # 'egg_info': CustomEggInfoCommand,
             'bdist_wheel': CustomBdistWheelCommand,
             'install': CustomInstallCommand,
         },
